8d6076f594b4f78fc8e55076f5175b6f105488d8
Luke Alexander <23AlexL@live.elida.k12.oh.us>
2020-12-15 14:17:42 -0500

    Cleaned Up Got Towers in with Money

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+
+    public float timeBetewwnWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetewwnWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+
+
+    void SpawnWave()
+    {
+      
+        for (int i = 0; i < waveNumber; i++)
+        
+        Debug.Log("Wave Incoming");
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
+
+
+
+
+
+
+   
+
