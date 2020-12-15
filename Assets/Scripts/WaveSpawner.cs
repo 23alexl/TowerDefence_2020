5a48c96c29b5bf38cbe74c956ce7847947cb7978
Luke Alexander <23AlexL@live.elida.k12.oh.us>
2020-12-15 14:22:23 -0500

    Made Animations and Death Animations

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,50 +1,71 @@
-﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour {
 
-    public Transform enemyPrefab;
+	public static int EnemiesAlive = 0;
 
-    public Transform spawnPoint;
+	public Wave[] waves;
 
+	public Transform spawnPoint;
 
-    public float timeBetewwnWaves = 5f;
-    private float countdown = 2f;
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
 
-    private int waveNumber = 1;
+	public Text waveCountdownText;
 
-    void Update()
-    {
-        if (countdown <= 0f)
-        {
-            SpawnWave();
-            countdown = timeBetewwnWaves;
-        }
+	public GameManager gameManager;
 
-        countdown -= Time.deltaTime;
-    }
+	private int waveIndex = 0;
 
+	void Update ()
+	{
+		if (EnemiesAlive > 0)
+		{
+			return;
+		}
 
+		if (waveIndex == waves.Length)
+		{
+			gameManager.WinLevel();
+			this.enabled = false;
+		}
 
-    void SpawnWave()
-    {
-      
-        for (int i = 0; i < waveNumber; i++)
-        
-        Debug.Log("Wave Incoming");
-        
-    }
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+			return;
+		}
 
-    void SpawnEnemy()
-    {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
-    }
+		countdown -= Time.deltaTime;
+
+		countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+
+		waveCountdownText.text = string.Format("{0:00.00}", countdown);
+	}
+
+	IEnumerator SpawnWave ()
+	{
+		PlayerStats.Rounds++;
+
+		Wave wave = waves[waveIndex];
+
+		EnemiesAlive = wave.count;
+
+		for (int i = 0; i < wave.count; i++)
+		{
+			SpawnEnemy(wave.enemy);
+			yield return new WaitForSeconds(1f / wave.rate);
+		}
+
+		waveIndex++;
+	}
+
+	void SpawnEnemy (GameObject enemy)
+	{
+		Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+	}
 
 }
-
-
-
-
-
-
-   
-
